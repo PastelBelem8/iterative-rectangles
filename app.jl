238516424de32165e208e42631e459699896d313
PastelBelem8 <catarina.belem@ist.utl.pt>
2020-09-14 21:49:21 +0100

    Add Label Indexer, Fix some bugs

--- app.jl
+++ app.jl
@@ -26,26 +26,26 @@
 =#
 
 mutable struct Parameter{T}
-  value::T
+    value::T
 end
 
 (p::Parameter)() = p.value
 (p::Parameter)(newvalue) = p.value = newvalue
 
 function with(f, p, newvalue)
-  oldvalue = p()
-  p(newvalue)
-  try
-    f()
-  finally
-    p(oldvalue)
-  end
+    oldvalue = p()
+    p(newvalue)
+    try
+        f()
+    finally
+        p(oldvalue)
+    end
 end
 
 with(f, p, newvalue, others...) =
-  with(p, newvalue) do
-    with(f, others...)
-  end
+    with(p, newvalue) do
+        with(f, others...)
+    end
 
 # ------------------------------------------------------------------------
 # Task 1. Create an abstraction - Rectangle
@@ -62,15 +62,15 @@ with(f, p, newvalue, others...) =
 # - texture: Enum, the texture of the rectangle (to add)
 
 @enum Color begin
-    black  = 1
-    blue   = 2
-    red    = 3
+    black = 1
+    blue = 2
+    red = 3
     orange = 4
     yellow = 5
-    green  = 6
+    green = 6
     purple = 7
-    brown  = 8
-    pink   = 9
+    brown = 8
+    pink = 9
 end
 
 struct Rectangle
@@ -80,14 +80,15 @@ struct Rectangle
     height::Real
     color::Color
     Rectangle(x, y, width, height, color) =
-      width <= 0 || height <= 0 ?
-        error("width and height must be positive") : new(x, y, width, height, color)
+        width <= 0 || height <= 0 ? error("width and height must be positive") :
+        new(x, y, width, height, color)
 end
 
 Rectangle() = Rectangle(0, 0)
 Rectangle(x, y) = Rectangle(x, y, 1, 1, black)
 
 n_features(::Type{Rectangle}) = 5
+categoricals(::Type{Rectangle}) = [5]
 
 # Implementation detail Note:
 # ---------------------------
@@ -105,9 +106,8 @@ MIN = Parameter(0)
 MAX = Parameter(1000)
 
 Random.rand(rng::AbstractRNG, ::Random.SamplerType{Rectangle}) =
-    let vals = rand(2),
-        vals = MIN() .+ vals .* (MAX() - MIN())
-        vals = round.(vals, digits=2)
+    let vals = rand(2), vals = MIN() .+ vals .* (MAX() - MIN())
+        vals = round.(vals, digits = 2)
         color = rand(instances(Color), 1)[1]
 
         Rectangle(0, 0, vals..., color)
@@ -133,7 +133,7 @@ rand(Rectangle, 2)
 # - Store
 
 # Type alias
-const Literal = Union{String, Real}
+const Literal = Union{String,Real}
 
 abstract type Indexer end
 
@@ -146,13 +146,14 @@ function transform(indexer::Indexer, feature) end
 
 mutable struct OneHotIndexer <: Indexer
     feature_id::Literal
-    values::Dict{Literal, String}
+    values::Dict{Literal,String}
 end
 
 OneHotIndexer(feature_id::Literal, feature) =
     let u_values = unique(feature),
         u_ids = 1:length(unique_values),
         values = map((v, id) -> v => id, u_values, u_ids)
+
         OneHotIndexer(feature_id, Dict(values))
     end
 
@@ -172,14 +173,16 @@ transform(indexer::OneHotIndexer, feature) =
 
 
 mutable struct LabelIndexer <: Indexer
-        feature_id::Literal
-        values::Dict{Literal, Int}
+    feature_id::Literal
+    values::Dict{Literal,Int}
 end
 
 LabelIndexer(feature_id::Literal, feature) =
     let u_values = unique(feature),
         u_ids = 1:length(unique_values),
         values = map((v, id) -> v => id, u_values, u_ids)
+
+        print(feature_id, Dict(values))
         LabelIndexer(feature_id, Dict(values))
     end
 
@@ -200,28 +203,30 @@ transform(indexer::LabelIndexer, feature) =
 # Additionally, Base.push! is specialized for a Dataset instance and is able
 # to add any object <: Shape to the specified dataset.
 mutable struct Dataset
-    X:: Matrix{Any}
-    y:: Matrix{Int64}
+    X::Matrix{Any}
+    y::Matrix{Int64}
     categoricals::Vector{Int64}
 end
 
-Dataset(n_features, n_labels) =
-    let X = Array{Any, 2}(undef, 0, n_features),
-        y = Array{Int64, 2}(undef, 0, n_labels)
-        Dataset(X, y)
+Dataset(n_features, n_labels, categoricals) =
+    let X = Array{Any,2}(undef, 0, n_features),
+        y = Array{Int64,2}(undef, 0, n_labels)
+
+        Dataset(X, y, categoricals)
     end
 
-n_features(data::Dataset) = size(data, 2)
-n_rows(data::Dataset) = size(data, 1)
+n_features(data::Dataset) = size(data.X, 2)
+n_rows(data::Dataset) = isempty(data) ? 0 : size(data.X, 1)
 
 categoricals(data::Dataset) = data.categoricals
 numericals(data::Dataset) = setdiff(1:n_features(data), categoricals(data))
 
 get_data(data::Dataset) = data.X
 get_label(data::Dataset) = data.y
-get_feature(name, data::Dataset) = data.X[:,name]
-get_features_names(data::Dataset, except=nothing) =
-    except === nothing ? Set(1:n_features(data)) : setdiff(Set(1:n_features(data)), Set(except))
+get_feature(name, data::Dataset) = data.X[:, name]
+get_features_names(data::Dataset, except = nothing) =
+    except === nothing ? Set(1:n_features(data)) :
+    setdiff(Set(1:n_features(data)), Set(except))
 
 import Base: push!, isempty
 
@@ -240,48 +245,63 @@ filter_by_y(data::Dataset, predicate) =
         let X = data.X,
             y = data.y,
             n_instances = size(X, 1),
-            ixs = filter((i) -> predicate(y[i,:]), 1:n_instances)
-            Dataset(X[ixs,:], y[ixs,:])
-         end
+            ixs = filter((i) -> predicate(y[i, :]), 1:n_instances)
+
+            Dataset(X[ixs, :], y[ixs, :], categoricals(data))
+        end
     end
 
 using DelimitedFiles
 filename = Parameter("data.csv")
 
 write(dataset::Dataset) =
-    let X = dataset.X,
-        y = dataset.y
+    let X = dataset.X, y = dataset.y
         open(filename(), "w") do io
             writedlm(io, [X y], ',')
         end
     end
 
-transform(indexer::Indexer, feature_id, Xs...) =
-    let all_features = size(2, Xs[1]),
+transform(indexer::T, feature_id, Xs...) where T =
+    let all_features = size(Xs[1], 2),
         features = map(X -> X[:, feature_id], Xs),
-        indexer = indexer(feature_id, features[1]),
-
+        indexer = indexer[1],
+        indexer = indexer(feature_id, features[1]) #=,
         transf_features = map((f) -> transform(indexer, f), features),
         non_transf_features = setdiff(Set(1:all_features), feature_id),
+        non_transf_features = collect(non_transf_features),
 
         transf_matrices = map(X -> X[:, non_transf_features], Xs),
-        transf_matrices = map((M, T) -> [M T], transf_matrices, transf_features)
+        transf_matrices =
+            map((M, T) -> [M T], transf_matrices, transf_features)
 
         transf_matrices
+=#
     end
 
-transform(indexers::Vector{Indexer}, features_id, Xs) =
-    foldl((Xs, (ix, f_id)) -> transform(ix, f_id, Xs), zip(indexers, features_id), Xs)
+
+transform(indexers::Vector{T}, features_id, Xs) where T = foldl(
+    (Xs, (ix, f_id)) -> transform(ix, f_id, Xs),
+    zip(indexers, features_id),
+    Xs,
+)
 
 #
 #   Main Loop
 #
-iterative_rectangles(shape, generate, draw, prompt, parser, seed::Int, max_attempts::Int) =
+iterative_rectangles(
+    shape,
+    generate,
+    draw,
+    prompt,
+    parser,
+    seed::Int,
+    max_attempts::Int,
+) =
     let label = "",
         n_features = n_features(shape),
-        data = Dataset(n_features, 1)
+        data = Dataset(n_features, 1, categoricals(shape))
 
-        for i in 1:max_attempts
+        for i = 1:max_attempts
             let object = generate(data, i, shape)
                 draw(object)
                 label = parser(prompt())
@@ -320,10 +340,11 @@ simple_parser(input::String) = tryparse(Int64, input)
 generate_random(dataset, iter, object_type) = rand(object_type)
 
 
+
+#=
 using Random
 Random.seed!(1234567)
 
-#=
 with(filename, "data_random.csv") do
     Random.seed!(1234567)
     iterative_rectangles(
@@ -336,43 +357,56 @@ end
 #     cosine similarity to those already liked by the user.
 using Distances
 
+bootstrap_samples = Parameter(1)
 n_samples = Parameter(50)
 distance_metric = Parameter(CosineDist())
 indexers = Parameter(Dict(
-        5 => LabelIndexer, # Color (feature 5) will be indexed
+    5 => LabelIndexer, # Color (feature 5) will be indexed
 ))
+
 
 # Generate samples
 # fit_transform dataset
 # transform samples (if necessary)
 # compute pairwise similarity between dataset and samples
 generate_similar(dataset, iter, object_type) =
-    let n_samples = n_samples(),
-        samples = rand(object_type, n_samples),
-        S = vcat(map(as_array, samples)...),
-        dataset = filter_by_y(dataset, (y) -> y == 1),
-        X = get_data(dataset),
-        y = get_label(dataset),
-        # Indexing
-        (features, idxs) = indexers(),
-        (X_indexed, samples_indexed) = transform(idxs, features, X, S),
-        # This yields an sm x s matrix
-        dist_metric = distance_metric(),
-        distances = pairwise(dist_metric, X_indexed', samples_indexed'),
-        distances_sum = mapslices(sum, distances, dims=2),
+    if n_rows(dataset) <= bootstrap_samples()
+        generate_random(dataset, iter, object_type)
+    else
+        let n_samples = n_samples(),
+            samples = rand(object_type, n_samples),
+            S = vcat(map(as_array, samples)...),
+            dataset = filter_by_y(dataset, (y) -> y == 1),
+            X = get_data(dataset),
+            y = get_label(dataset),
+            # Indexing
+            features_lst = collect(keys(indexers())),
+            indexers_lst = map(f -> get(indexers(), f, nothing), features_lst),
+            (X_indexed, samples_indexed) = transform(indexers_lst, features_lst, (X, S)...),
+            # This yields an sm x s matrix
+            dist_metric = distance_metric(),
+            distances = pairwise(dist_metric, X_indexed', samples_indexed'),
+            distances_sum = mapslices(sum, distances, dims = 2),
 
-        # Pick
-        sample_ix = argmax(distances_sum)
-        samples[sample_ix]
+            # Pick
+            sample_ix = argmax(distances_sum)
+            samples[sample_ix]
+        end
     end
+
 
 with(filename, "data_similar.csv") do
     Random.seed!(1234567)
     iterative_rectangles(
-        Rectangle, generate_similar, cli_draw, cli_input, simple_parser, 42, 1000
+        Rectangle,
+        generate_similar,
+        cli_draw,
+        cli_input,
+        simple_parser,
+        42,
+        1000,
     )
 end
-#
 
 # There are many
 #     - EMA (weight different models based on the most recent)
